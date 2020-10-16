4e3028f8ec3aaf54e18b8cde2c0ce1351818da18
Freewind <nowindlee@gmail.com>
2020-10-16 21:35:08 +0800

    typescript-typing-typed-path-in-array-demo

--- hello.ts
+++ hello.ts
@@ -1,19 +1,13 @@
-type CompassFormState = {
-  agentNames?: string,
-  normalizedFilters?: {
-    meow?: string
-  }
-  complexNames?: {
-    name?: string
-  }[]
-}
-
 type Cons<H, T> = T extends readonly any[] ?
   ((h: H, ...t: T) => void) extends ((...r: infer R) => void) ? R : never
   : never;
 
-type Prev = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-  11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...0[]]
+type Prev = [
+  never,
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  // https://stackoverflow.com/questions/64356569/how-to-understand-0-1-2-0-in-type-definition-in-typescript
+  ...0[]
+]
 
 type Paths<T, D extends number = 10> = [D] extends [never]
   ? never
@@ -26,17 +20,27 @@ type Paths<T, D extends number = 10> = [D] extends [never]
         : never
         ))
     }[keyof T]
-    : [];
+    : [] ;
 
-type CompassFormPaths = {
-  [key: string]: Paths<CompassFormState, 3>; // << I set the max depth to 3, the author defaulted to 10 which is pretty deep
+type FormState = {
+  userName?: string,
+  filters?: {
+    meow?: string
+  }
+  complexNames?: {
+    name?: string
+  }[]
 }
 
-export const CompassQueryKeys: CompassFormPaths = {
-  AGENT_NAME: ['agentNames'],
-  BUILDING_ADDRESSES: ['normalizedFilters', 'meow'], // << Throws a type error
-  SOME_NAME: ['complexNames', 0, 'name'],
+type FormPaths = {
+  [key: string]: Paths<FormState>;
 }
 
-console.log(CompassQueryKeys)
+export const queryPaths: FormPaths = {
+  USER_NAME: ['userName'],
+  MEOW: ['filters', 'meow'], // << Throws a type error
+  COMPLEX_NAMES: ['complexNames', 0, 'name'],
+}
+
+console.log(queryPaths);
 
